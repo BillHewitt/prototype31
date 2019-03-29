0885ec7d3bf31eeb92179d075e28f064d7fa9983
Bill Hewitt <billhewitt22@googlemail.com>
2019-03-29 03:39:45 +0000

    ready

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,120 @@
+class PagesController < ApplicationController
+  def main
+  end
+
+  def login
+  end
+
+  def configure
+  end
+
+  def motivators
+  end
+
+  def welcome
+  end
+
+  def welcome2
+  end
+
+  def credit
+  end
+
+  def companyperks
+  end
+
+  def performanceperks
+  end
+
+  def performanceperksman
+  end
+
+  def financial
+  end
+
+  def market
+  end
+
+  def gift
+  end
+
+  def financialman
+  end
+
+  def marketman
+  end
+
+  def giftman
+  end
+
+
+  def feed
+  end
+
+  def feedman
+  end
+
+  def feed1
+  end
+
+  def welcomeman
+  end
+
+  def welcomeman2
+  end
+
+  def creditman
+  end
+
+  def creditteam
+  end
+
+  def team
+  end
+
+  def team2
+  end
+
+  def companyperksman
+  end
+
+  def profile
+  end
+
+  def history
+  end
+
+  def trophies
+  end
+
+  def help
+  end
+
+  def welcome3
+  end
+
+  def watched
+  end
+
+  def welcome4
+  end
+
+  def welcome5
+  end
+
+  def welcome6
+  end
+
+  def login2
+  end
+
+  def motivators2
+  end
+
+  def configure2
+  end
+
+  def physical
+  end
+
+end
